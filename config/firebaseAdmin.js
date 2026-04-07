c58ef3f25c5e2085327b7c40d5e411531265b874
natchaya <s6504062636136@email.kmutnb.ac.th>
2026-04-07 10:21:27 +0700

    use firebase admin from env

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,16 +1,14 @@
 import admin from "firebase-admin";
-import "firebase-admin/auth";
-import "firebase-admin/storage";
-import fs from "fs";
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync("./config/serviceAccountKey.json", "utf8")
-);
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+};
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET || undefined,
   });
 }
 
